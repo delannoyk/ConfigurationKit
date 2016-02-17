e624dc71ceb9e77555c7c105d516e1514fc91476
Kevin Delannoy <delannoyk@gmail.com>
2016-02-17 18:45:44 +0100

    Added ConfigurationEventListener

--- sources/ConfigurationKit/configuration/InternalEventListener.swift
+++ sources/ConfigurationKit/configuration/InternalEventListener.swift
@@ -13,20 +13,26 @@ import Foundation
  *  it isn't public. It allows `Configuration` to implement it
  *  internally and doesn't show publicly the implementation.
  */
-internal protocol InternalEventListener {
+internal protocol InternalEventListener: class {
     /**
      Called when an event occurs.
      */
     func onEvent()
 }
 
-/*internal class InternalEventListener: EventListener {
-
+/**
+ *  `ConfigurationEventListener` is an internal implementation
+ *  of `EventListener` so that we don't have to make the implementation
+ *  public.
+ */
+internal class ConfigurationEventListener: EventListener {
+    /// The real event listener
+    weak var realEventListener: InternalEventListener?
 
     /**
      Called when an event occurs.
      */
     func onEvent() {
-
+        realEventListener?.onEvent()
     }
-}*/
+}
