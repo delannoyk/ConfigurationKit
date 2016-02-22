118c7aeac5397ac2595d84f256a91ce3970a7cd5
Kevin Delannoy <delannoyk@gmail.com>
2016-02-22 19:25:33 +0100

    Working on Configuration test case

---
+++ sources/ConfigurationKitTests/tests/configuration/Configuration_TestCase.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration_TestCase.swift
+//  ConfigurationKit
+//
+//  Created by Kevin DELANNOY on 22/02/16.
+//  Copyright © 2016 Kevin Delannoy. All rights reserved.
+//
+
+import XCTest
+@testable import ConfigurationKit
+
+class Configuration_TestCase: XCTestCase {
+
+}
