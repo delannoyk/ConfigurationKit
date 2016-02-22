aa8ef55ce1f64b7ca37ae03c71742aa5a72fd90a
Kevin Delannoy <delannoyk@gmail.com>
2016-02-22 21:52:05 +0100

    Added tests for URLSessionDownloader

--- sources/ConfigurationKitTests/tests/downloader/URLSessionDownloader_TestCase.swift
+++ sources/ConfigurationKitTests/tests/downloader/URLSessionDownloader_TestCase.swift
@@ -9,6 +9,82 @@
 import XCTest
 @testable import ConfigurationKit
 
+class DataTask: NSURLSessionDataTask {
+    var completion: ((NSData?, NSURLResponse?, NSError?) -> Void)?
+    var call = true
+
+    override func resume() {
+        if call { completion?(nil, nil, nil) }
+    }
+
+    override func cancel() {
+    }
+}
+
+class URLSession: NSURLSession {
+    var call = true
+
+    override func dataTaskWithRequest(request: NSURLRequest, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) -> NSURLSessionDataTask {
+        let d = DataTask()
+        d.completion = completionHandler
+        d.call = call
+        return d
+    }
+}
+
 class URLSessionDownloader_TestCase: XCTestCase {
-    
+    let urlSession = URLSession()
+
+    func testCompletionGetsCalled() {
+        let expectation = expectationWithDescription("Wait for download data completion")
+        let downloader = URLSessionDownloader(session: urlSession,
+            responseQueue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))
+
+        urlSession.call = true
+
+        downloader.downloadData(NSURLRequest()) { data, error in
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(1) { error in
+            XCTAssertNil(error)
+        }
+    }
+
+    func testHasPendingRequestIsTrue() {
+        let downloader = URLSessionDownloader(session: urlSession,
+            responseQueue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))
+
+        urlSession.call = false
+
+        downloader.downloadData(NSURLRequest()) { data, error in
+            XCTFail()
+        }
+        XCTAssert(downloader.hasPendingRequest)
+    }
+
+    func testNewDownloadCancelPreviousOne() {
+        let downloader = URLSessionDownloader(session: urlSession,
+            responseQueue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))
+
+        urlSession.call = false
+
+        let expectationFailure = expectationWithDescription("Wait for download data completion")
+        downloader.downloadData(NSURLRequest()) { data, error in
+            XCTAssertNotNil(error)
+            expectationFailure.fulfill()
+        }
+
+        urlSession.call = true
+
+        let expectationSuccess = expectationWithDescription("Wait for download data completion")
+        downloader.downloadData(NSURLRequest()) { data, error in
+            XCTAssertNil(error)
+            expectationSuccess.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(1) { error in
+            XCTAssertNil(error)
+        }
+    }
 }
