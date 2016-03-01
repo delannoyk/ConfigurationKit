acf7487d2b2ec378f1ded062db73aa004d4d359f
Kevin Delannoy <delannoyk@gmail.com>
2016-03-01 21:28:29 +0100

    Changed ApplicationEventProducer and removed #if

--- sources/ConfigurationKit/event/ApplicationEventProducer.swift
+++ sources/ConfigurationKit/event/ApplicationEventProducer.swift
@@ -6,68 +6,62 @@
 //  Copyright © 2016 Kevin Delannoy. All rights reserved.
 //
 
-//FIXME: I'm looking for a better way to express that it isn't available on
-//watchOS. maybe @unavailable?
-#if os(tvOS) || os(iOS) || os(OSX)
+import UIKit
 
-    import UIKit
+/**
+ *  A `EventProducer` that listens to when application enters in foreground and generates an
+ *  event from that.
+ */
+public class ApplicationEventProducer: NSObject, EventProducer {
+    /// A boolean value indicating whether we're already generating events or not.
+    private var started = false
+
+    /// The event listener.
+    public weak var eventListener: EventListener?
 
     /**
-     *  A `EventProducer` that listens to when application enters in foreground and generates an
-     *  event from that.
+     Stops producing events on deinit.
      */
-    public class ApplicationEventProducer: NSObject, EventProducer {
-        /// A boolean value indicating whether we're already generating events or not.
-        private var started = false
-
-        /// The event listener.
-        public weak var eventListener: EventListener?
-
-        /**
-         Stops producing events on deinit.
-         */
-        deinit {
-            stopProducingEvents()
-        }
-
-        /**
-         Listens to application notifications and generates events each time one of these
-         notification gets fired.
-         */
-        public func startProducingEvents() {
-            guard !started else {
-                return
-            }
-
-            NSNotificationCenter.defaultCenter().addObserver(self,
-                selector: "applicationWillEnterForeground:",
-                name: UIApplicationWillEnterForegroundNotification,
-                object: UIApplication.sharedApplication())
-            started = true
-        }
-
-        /**
-         Stops listening to registered notifications.
-         */
-        public func stopProducingEvents() {
-            guard started else {
-                return
-            }
-
-            NSNotificationCenter.defaultCenter().removeObserver(self,
-                name: UIApplicationWillEnterForegroundNotification,
-                object: UIApplication.sharedApplication())
-            started = false
-        }
-
-        /**
-         Method that gets called when `UIApplicationWillEnterForegroundNotification` is fired.
-
-         - parameter note: The notification.
-         */
-        @objc private func applicationWillEnterForeground(note: NSNotification) {
-            eventListener?.onEvent()
-        }
+    deinit {
+        stopProducingEvents()
     }
 
-#endif
+    /**
+     Listens to application notifications and generates events each time one of these
+     notification gets fired.
+     */
+    public func startProducingEvents() {
+        guard !started else {
+            return
+        }
+
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "applicationWillEnterForeground:",
+            name: UIApplicationWillEnterForegroundNotification,
+            object: UIApplication.sharedApplication())
+        started = true
+    }
+
+    /**
+     Stops listening to registered notifications.
+     */
+    public func stopProducingEvents() {
+        guard started else {
+            return
+        }
+
+        NSNotificationCenter.defaultCenter().removeObserver(self,
+            name: UIApplicationWillEnterForegroundNotification,
+            object: UIApplication.sharedApplication())
+        started = false
+    }
+
+    /**
+     Method that gets called when `UIApplicationWillEnterForegroundNotification` is fired.
+
+     - parameter note: The notification.
+     */
+    @objc private func applicationWillEnterForeground(note: NSNotification) {
+        eventListener?.onEvent()
+    }
+}
